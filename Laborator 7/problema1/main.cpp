840fba3f6ca0fb516284c4daf902818aa2514c0b
ediionut23 <ediionut23@gmail.com>
2024-04-24 15:52:36 +0300

    miauv2

--- Laborator 7/problema1/main.cpp
+++ Laborator 7/problema1/main.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 using namespace std;
 
-float operator "" _Kelvin(long double temp) {
+float operator "" _Kelvin(unsigned long long temp) {
     return (float)(temp - 273.15);
 }
 
-float operator "" _Fahrenheit(long double temp) {
+float operator "" _Fahrenheit(unsigned long long temp) {
     return (float)((temp - 32) * 5 / 9);
 }
 
 int main() {
 
-    float a = 300.0_Kelvin;
-    float b = 120.0_Fahrenheit;
+    float a = 300_Kelvin;
+    float b = 120_Fahrenheit;
 
     cout << a << " " << b;
 
