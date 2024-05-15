347038f3f5e6b9331ae39ea126629bbda115ad11
ediionut23 <ediionut23@gmail.com>
2024-05-15 13:00:49 +0300

    c

--- Laborator 8/main.cpp
+++ Laborator 8/main.cpp
@@ -4,15 +4,22 @@
 
 using namespace std;
 
+bool Mare(char c){
+    return c >= 'A' && c <= 'Z';
+}
+
 int main() {
     string s = "I bought an apple. Then I eat an apple. Apple is my favorite.";
     string myWord = "";
     map<string, int> myMap;
     for(auto i : s){
         if(i == ' ' || i == ',' || i == '.'){
-            myMap[myWord]++;
+            if(myWord != "" || myWord != "\0")
+                myMap[myWord]++;
             myWord="";
         }else{
+            if(Mare(i))
+                i += 32;
             myWord = myWord + i;
         }
     }
